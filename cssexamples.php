0a5b42b38eaff99c11bab0e425bfac3c8d099d14
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-10-23 16:12:43 -0600

    Add files via upload

---
+++ cssexamples.php
@@ -0,0 +1,16 @@
+<?php
+    echo <<<_INIT
+        <!DOCTYPE html> 
+            <head>
+                <link rel='stylesheet' href='styles.css' type='text/css'>
+            </head>
+                <div class='center>
+                    <body class='body'>body</body>
+                    <h1>h1</h1>
+                    <h2>h2</h2>
+                    <a>a</a>
+                </div>
+        </html>
+    _INIT
+?>
+
