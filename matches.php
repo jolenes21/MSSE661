804d9dde352443ba521b8a0e903b522a306ef292
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-10-23 16:30:29 -0600

    Add files via upload

--- matches.php
+++ matches.php
@@ -1,86 +1,82 @@
 <?php // matches.php
-  require_once 'header.php';
+require_once 'header.php';
 
-  if (!$loggedin) die("</div></body></html>");
+    if (!$loggedin) die("</div></body></html>");
 
-  if (isset($_GET['view'])) $view = sanitizeString($_GET['view']);
-  else                      $view = $username;
+    if (isset($_GET['view'])) $view = sanitizeString($_GET['view']);
+    else                      $view = $username;
 
-  if ($view == $username)
-  {
-    $name1 = $name2 = "Your";
-    $name3 =          "You are";
-  }
-  else
-  {
-    $name1 = "<a data-transition='slide'
-              href='members.php?view=$view'>$view</a>'s";
-    $name2 = "$view's";
-    $name3 = "$view is";
-  }
+    if ($view == $username)
+    {
+        $name1 = $name2 = "Your";
+        $name3 =          "You are";
+    }
+    else
+    {
+        $name1 = "<a data-transition='slide' href='members.php?view=$view'>$view</a>'s";
+        $name2 = "$view's";
+        $name3 = "$view is";
+    }
 
-  // Uncomment this line if you wish the user�s profile to show here
-  // showProfile($view);
+// Uncomment this line if you wish the user�s profile to show here
+// showProfile($view);
 
-  $followers = array();
-  $following = array();
+$followers = array();
+$following = array();
 
-  $result = queryMysql("SELECT * FROM matches WHERE baseMemberID=(SELECT memberID FROM members WHERE username ='$view')");
-  $num    = $result->num_rows;
+$result = queryMysql("SELECT * FROM matches WHERE baseMemberID=(SELECT memberID FROM members WHERE username ='$view')");
+$num    = $result->num_rows;
 
-  for ($j = 0 ; $j < $num ; ++$j)
-  {
-    $row           = $result->fetch_array(MYSQLI_ASSOC);
-    $followers[$j] = $row['matchMemberID'];
-  }
+    for ($j = 0 ; $j < $num ; ++$j)
+    {
+        $row           = $result->fetch_array(MYSQLI_ASSOC);
+        $followers[$j] = $row['matchMemberID'];
+    }
 
-  $result = queryMysql("SELECT * FROM matches WHERE matchMemberID=(SELECT memberID FROM members WHERE username='$view')");
-  $num    = $result->num_rows;
+$result = queryMysql("SELECT * FROM matches WHERE matchMemberID=(SELECT memberID FROM members WHERE username='$view')");
+$num    = $result->num_rows;
 
-  for ($j = 0 ; $j < $num ; ++$j)
-  {
-      $row           = $result->fetch_array(MYSQLI_ASSOC);
-      $following[$j] = $row['username'];
-  }
+    for ($j = 0 ; $j < $num ; ++$j)
+    {
+        $row           = $result->fetch_array(MYSQLI_ASSOC);
+        $following[$j] = $row['baseMemberID'];
+    }
 
-  //$mutual    = array_intersect($followers, $following);
-  $followers = array_diff($followers, $mutual);
-  $following = array_diff($following, $mutual);
-  $matches   = FALSE;
+$mutual    = array_intersect($followers, $following);
+$followers = array_diff($followers/*, $mutual*/);
+$following = array_diff($following/*, $mutual*/);
+$matches   = FALSE;
 
-  echo "<br>";
-  
-  /*if (sizeof($mutual))
-  {
-    echo "<span class='subhead'>$name2 mutual friends</span><ul>";
-    foreach($mutual as $friend)
-      echo "<li><a data-transition='slide'
-            href='members.php?view=$friend'>$friend</a>";
-    echo "</ul>";
-    $matches = TRUE;
-  }*/
+echo "<br>";
 
-  if (sizeof($followers))
-  {
+    if (sizeof($mutual))
+    {
+        echo "<span class='subhead'>$name2 matches</span><ul>";
+        foreach($mutual as $match)
+        echo "<li><a data-transition='slide' href='members.php?view=$match'>$match</a>";
+        echo "</ul>";
+      $matches = TRUE;
+    }
+
+if (sizeof($followers))
+{
     echo "<span class='subhead'>$name2 followers</span><ul>";
     foreach($followers as $friend)
-      echo "<li><a data-transition='slide'
-            href='members.php?view=$friend'>$friend</a>";
+    echo "<li><a data-transition='slide' href='members.php?view=$match'>$match</a>";
     echo "</ul>";
     $matches = TRUE;
-  }
+}
 
-  if (sizeof($following))
-  {
-    echo "<span class='subhead'>$name3 following</span><ul>";
+if (sizeof($following))
+{
+    echo "<span class='subhead'>$name3 maybes</span><ul>";
     foreach($following as $match)
-      echo "<li><a data-transition='slide'
-            href='members.php?view=$match'>$match</a>";
+    echo "<li><a data-transition='slide' href='members.php?view=$match'>$match</a>";
     echo "</ul>";
     $matches = TRUE;
-  }
+}
 
-  if (!$matches) echo "<br>You don't have any matches yet.";
+if (!$matches) echo "<br>You don't have any matches yet.";
 ?>
     </div><br>
   </body>
