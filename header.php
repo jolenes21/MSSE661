43e7e68f5a76976a72e0fd0955b4afd6049b29b2
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-10-23 16:16:12 -0600

    Add files via upload

--- header.php
+++ header.php
@@ -18,12 +18,16 @@ echo <<<_INIT
 <html>
   <head>
     <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'> 
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:900|Work+Sans:300" rel="stylesheet">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:900|Work+Sans:300' rel="stylesheet">
     <link rel='stylesheet' href='styles.css' type='text/css'>
+    <script src='//jsfiddle.net/vRqcb/11/'></script>
     <script src='javascript.js'></script>
-    <script async src="//jsfiddle.net/vRqcb/11/"></script>
-
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 
 _INIT;
 
@@ -42,17 +46,17 @@ _INIT;
 echo <<<_MAIN
     <title>Approv.d $userstr</title>
   </head>
-  <body class='body'>
-    <div data-role='page'>
-      <h1>
-        <div data-role='header'>
-          <div class='center'>
-              Approv.d
-          </div><br>
-        </div>
-      </h1>
-    <div data-role='content'>
-  </body>
+    <body class='body'>
+      <div data-role='page'>
+        <h1>
+          <div data-role='header'>
+            <div class='center'>
+                Approv.d
+            </div><br>
+          </div>
+        </h1>
+      <div data-role='content'>
+    </body>
 _MAIN;
 
   if ($loggedin)
@@ -61,23 +65,22 @@ echo <<<_LOGGEDIN
         <body class='h3'>
             <h3>
                 <div class='center'>
-                    <ul id='tabs'>
-                        <li><a id='home' data-inline='true' data-transition='slide' 
-                            href='members.php?view=$username'>Home</a></li>
-                        <li><a id='maybes' data-inline='true' data-transition='slide' 
-                            href='members.php'>Maybes</a></li>
-                        <li><a id='matches' data-inline='true' data-transition='"slide" '
-                            href='matches.php'>Matches</a></li>
-                        <li><a id='matchers' data-inline='true' data-transition='slide' 
-                            href='underconstruction.php'>Matchers</a></li>
-                        <li><a id='matchers' data-inline='true' data-transition='slide'
-                            href='messages.php'>Messages</a></li>
-                        <li><a id='matchers' data-inline='true' data-transition='slide'
-                            href='profile.php'>Edit Profile</a></li>
-                        <li><a id='matchers' data-inline='true' data-transition='slide'
-                            href='logout.php'>Log out</a></li>
+                    <ul id="tabs">
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab1" class="" href='members.php?view=$username'>Home</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab2" class="" href='members.php'>Maybes</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab3" href='matches.php'>Matches</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab4" href='underconstruction.php'>Matchers</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab5" href='messages.php'>Messages</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab6" href='profile.php'>Edit Profile</a></li>
+                        <li><a data-role='button' data-inline='true' data-transition='slide' 
+                            id="tab7" href='logout.php'>Log out</a></li>
                     </ul>
-                  </div><br>
                 </div>
             </h3>
         </body>
