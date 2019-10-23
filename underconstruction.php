4f708856ae5de48fcdc2c09a8f6fcfdec966d47a
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-10-23 17:22:18 -0600

    Add files via upload

---
+++ underconstruction.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'header.php';
+
+echo <<<_END
+
+<html>
+    <body>
+        <div class='center'>This page is under construction.<br><br>
+            Good stuff coming soon.<br><br>
+        </div>
+        <div class='center'>
+            <a data-role='button' data-inline='true' data-icon='check'
+            data-transition="slide" href='index.php'>Home</a>
+        </div>
+  </body>
+</html>
+_END;
+
+?>
