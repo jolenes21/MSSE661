569725fdca59cfdb5d6bf9a91cb3271f0ec6bbd3
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-10-23 16:20:08 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php // index.php
+  session_start();
+require_once 'header.php';
+
+echo <<<_END
+
+<div id="page-container">
+    </div>
+    <div class='center'>
+      <footer id="footer">Jolene Lozow MSSE 661 2019</footer>
+    </div>
+  </body>
+</html>
+_END;
+?>
